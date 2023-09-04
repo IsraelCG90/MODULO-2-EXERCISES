d786d3c734eee219a44803aee4e800bff891d5f4
IsraelCG <israelcarrion.g@gmail.com>
2023-09-04 00:05:36 -0300

    ejercicios sprint2

--- 3. Condicionales/introJava.js
+++ 3. Condicionales/introJava.js
@@ -1,9 +1,9 @@
 /*Ejercicio 1*/
 function primerPrograma(x, y) {
-    if (x > y){
-        console.log(`${x} es mayor que ${y}`)
+    if (x > y) {
+        console.log(`${x} es mayor que ${y}`);
     } else {
-        console.log(`${x} no es mayor que ${y}`)
+        console.log(`${x} no es mayor que ${y}`);
     }
 }
 primerPrograma(5, 3);
@@ -11,7 +11,7 @@ primerPrograma(7, 1);
 
 /*Ejercicio 2*/
 function segundoPrograma(x, y) {
-    if (x == y){
+    if (x == y) {
         console.log(`${x} es igual a ${y}`)
     } else {
         console.log(`${x} es diferente a ${y}`)
@@ -36,9 +36,9 @@ tercerPrograma(7, 7);
 
 /*Ejercicio 4*/
 function cuartoPrograma(x, y, z) {
-    if (x < y && x < z){
+    if (x < y && x < z) {
         console.log(`${x} es el más chico`);
-    } else if (y < x && y < z){
+    } else if (y < x && y < z) {
         console.log(`${y} es el más chico`);
     } else {
         console.log(`${z} es el más chico`);
@@ -59,17 +59,142 @@ var personaDos = {
     edad: 50,
     altura: 175,
 };
-function quintoPrograma(obj1, obj2){
-    obj1.altura > obj2.altura ? 
-    console.log(`${obj1.nombre} es más alto que ${obj2.nombre}`): 
-    console.log(`${obj2.nombre} es más alto que ${obj1.nombre}`);
-    obj1.edad > obj2.edad ? 
-    console.log(`${obj1.nombre} es mayor que ${obj2.nombre}`): 
-    console.log(`${obj2.nombre} es mayor que ${obj1.nombre}`);
+function quintoPrograma(obj1, obj2) {
+    obj1.altura > obj2.altura ?
+        console.log(`${obj1.nombre} es más alto que ${obj2.nombre}`) :
+        console.log(`${obj2.nombre} es más alto que ${obj1.nombre}`);
+    obj1.edad > obj2.edad ?
+        console.log(`${obj1.nombre} es mayor que ${obj2.nombre}`) :
+        console.log(`${obj2.nombre} es mayor que ${obj1.nombre}`);
 }
 quintoPrograma(personaUno, personaDos);
 
 /*Ejercico 6*/
-
+function sextoPrograma() {
+    let nombre = prompt("Ingresa tu nombre");
+    let edad = Number(prompt("Ingresa tu edad"));
+    let altura = Number(prompt("Ingresa tu altura en cm"));
+    let vision = Number(prompt("Califica tu vision del 1 al 10"));
+    if (edad >= 18 && altura > 150 && vision >= 8) {
+        alert(`${nombre} estas capacitado para conducir`);
+    } else {
+        alert(`${nombre} no estas capacitado para conducir`);
+    }
+}
+//sextoPrograma()
 
 /*Ejercico 7*/
+function septimoPrograma() {
+    let nombre = prompt("Ingrese su nombre");
+    let pase = prompt("Indique su tipo de pase: vip o normal");
+    let entrada = prompt("Indique con 'si' o 'no' si posee entrada");
+    if (nombre == "Israel" || pase == "vip") {
+        alert("Bienvenido tiene ingreso libre");
+    } else if (entrada == "si") {
+        let respuesta = prompt("Desea utilizar su entrada?");
+        if (respuesta == "si") {
+            alert("Bienvenido tiene ingreso");
+        } else {
+            alert("Le esperamos pronto");
+        }
+    } else {
+        let compra = prompt("Desea compar una entrada?");
+        if (compra == "si") {
+            let dinero = prompt("De cuanto dinero dispone?");
+            if (dinero >= 1000) {
+                alert("La compra fue exitosa, bienvenido!");
+            } else {
+                alert("Su compra fue rechazada")
+            }
+        } else {
+            alert("Le esperamos pronto");
+        }
+    }
+}
+//septimoPrograma();
+
+/*Ejercico 8*/
+function adivinanza() {
+    alert("Tienes 3 intentos para adivinar un numero del 1 al 10. Suerte!");
+    let numeroIncognita = 6;
+    let numeroIngresado = prompt("Primer intento :)");
+    if (numeroIncognita == numeroIngresado){
+        alert("Ganaste, has adivinado el número.");
+    } else if (numeroIngresado > numeroIncognita) {
+        alert(`El numero por adivinar es menor a ${numeroIngresado}`);
+        numeroIngresado = prompt("Segundo intento");
+        if(numeroIncognita == numeroIngresado){
+            alert("Ganaste, has adivinado el número.");
+        } else if (numeroIngresado > numeroIncognita) {
+            alert(`El numero por adivinar es menor a ${numeroIngresado}`);
+            numeroIngresado = prompt("Tercer intento");
+            if(numeroIncognita == numeroIngresado){
+                alert("Ganaste, has adivinado el número.");
+            } else{
+                alert(`Lo siento perdiste, el numero era ${numeroIncognita}`);
+            }
+        } else {
+            alert(`El numero por adivinar es mayor a ${numeroIngresado}`);
+            numeroIngresado = prompt("Tercer intento");
+            if(numeroIncognita == numeroIngresado){
+                alert("Ganaste, has adivinado el número.");
+            } else{
+                alert(`Lo siento perdiste, el numero era ${numeroIncognita}`);
+            }
+        }
+    } else {
+        alert(`El numero por adivinar es mayor a ${numeroIngresado}`);
+        numeroIngresado = prompt("Segundo intento");
+        if(numeroIncognita == numeroIngresado){
+            alert("Ganaste, has adivinado el número.");
+        } else if (numeroIngresado > numeroIncognita) {
+            alert(`El numero por adivinar es mayor a ${numeroIngresado}`);
+            numeroIngresado = prompt("Tercer intento");
+            if(numeroIncognita == numeroIngresado){
+                alert("Ganaste, has adivinado el número.");
+            } else{
+                alert(`Lo siento perdiste, el numero era ${numeroIncognita}`);
+            }
+        } else {
+            alert(`El numero por adivinar es menor a ${numeroIngresado}`);
+            numeroIngresado = prompt("Tercer intento");
+            if(numeroIncognita == numeroIngresado){
+                alert("Ganaste, has adivinado el número.");
+            } else{
+                alert(`Lo siento perdiste, el numero era ${numeroIncognita}`);
+            }
+        }
+    }
+}
+//adivinanza();
+
+/*Ejercico 9*/
+function novenoPrograma(){
+    let edad = prompt("Ingresa tu edad");
+    if ( edad >= 0 && edad <= 12) {
+        alert("Eres un infante");
+    } else if (edad >= 13 && edad <= 18){
+        alert("Eres un adolecente");
+    } else if (edad >= 19 && edad <= 45){
+        alert("Eres un joven mayor");
+    } else if (edad > 45 && edad <= 100){
+        alert("Eres un anciano");
+    } else {
+        alert("De verdad tienes esa edad?");
+    }
+}
+//novenoPrograma();
+
+/*Ejercico 10*/
+function juego(){
+    let jugadorUno = prompt("Jugador 1 --> PIEDRA, PAPEL O TIJERA");
+    let jugadorDos = prompt("Jugador 2 --> PIEDRA, PAPEL O TIJERA");
+    if (jugadorUno == jugadorDos) {
+        alert("Empate")
+    } else if ((jugadorUno == "PIEDRA" && jugadorDos == "PAPEL") || (jugadorUno == "PAPEL" && jugadorDos == "TIJERA") || (jugadorUno == "TIJERA" && jugadorDos == "PIEDRA") ){
+        alert("Jugador 2 has ganado");
+    } else if ((jugadorUno == "PAPEL" && jugadorDos == "PIEDRA") || (jugadorUno == "TIJERA" && jugadorDos == "PAPEL") || (jugadorUno == "PIEDRA" && jugadorDos == "TIJERA") ){
+        alert("Jugador 1 has ganado");
+    } 
+}
+juego();
---
+++ 4. Bucles/introJava.js
@@ -0,0 +1,209 @@
+/*---------------------Ejercicio 1------------------------*/
+function programaUno() {
+  let num = prompt("Ingresa un numero del 1 y 100");
+  if (num > 0 && num <= 100) {
+    for (i = num; i > 0; i--) {
+      console.log(i);
+    }
+  } else {
+    alert("El numero ingresado es incorrecto");
+  }
+}
+//programaUno();
+
+/*---------------------Ejercicio 2------------------------*/
+
+function programaDos() {
+  let num = prompt("Ingrese un numero del 1 al 10");
+  if (num > 0 && num <= 10) {
+    for (i = 1; i < 11; i++) {
+      console.log(`${i} x ${num} = ${i * num}`);
+    }
+  } else {
+    alert("El numero ingresado es incorrecto");
+  }
+}
+//programaDos();
+
+/*---------------------Ejercicio 3------------------------*/
+function tercerPrograma() {
+  let num = Number(prompt("Ingrese un numero"));
+  let acumulador = num;
+  while (num > 0) {
+    num = Number(prompt("Ingrese un numero"));
+    acumulador = acumulador + num;
+  };
+  console.log(acumulador);
+};
+/*
+function tercerPrograma() {
+  let acumulador = 0;
+  let continuar = true;
+  while(continuar){
+    let numeroIngresado = Number(prompt("Ingrese un numero"));
+    acumulado += numeroIngresado
+    if(numeroIngresado == 0){
+      continuar = false;
+    };
+  };
+  console.log(`Despues del bucle el acumulado es ${acumulador}`);
+}*/
+//tercerPrograma();
+
+/*---------------------Ejercicio 4------------------------*/
+function cuartoPrograma() {
+  let num = Number(prompt("Ingrese un numero"));
+  let acumulador = num;
+  do {
+    num = Number(prompt("Ingrese un numero"));
+    acumulador += num;
+  } while (num != 0);
+  console.log(acumulador);
+}
+//cuartoPrograma();
+
+/*---------------------Ejercicio 5------------------------*/
+function quintoPrograma() {
+  let adivinar = 9;
+  let acumulador = 0;
+  let num = Number(prompt("Adivina el numero: ingresa tu opcion"));
+  while (num != adivinar) {
+    if (num > adivinar) {
+      acumulador++;
+      alert("El numero ingresado es muy grande");
+      num = Number(prompt("Adivina el numero: ingresa tu opcion"));
+    } else {
+      acumulador++;
+      alert("El numero ingresado es muy chico");
+      num = Number(prompt("Adivina el numero: ingresa tu opcion"));
+    }
+  }
+  alert(`Felicitaciones adivinaste! El numero de intentos fue: ${acumulador}`);
+}
+//quintoPrograma();
+
+/*---------------------Ejercicio 6------------------------*/
+function sextoPrograma() {
+  let num = Number(prompt("Ingrese un numero"));
+    // 50
+  for (let i = num; i > 0; i--) {
+              //50
+              //49
+              //48
+              //25
+    if(num % i == 0) {
+      //50  50 
+      //50  49
+      //50  48
+      //50  25  
+      console.log(i); //50 25 10  5  1 
+    };
+  }
+}
+//sextoPrograma();
+
+/*---------------------Ejercicio 7------------------------*/
+let colores = ["Rojo", "Verde", "Azul", "Violeta", "Amarillo", "Celeste", "Naranja", "Rosa"];
+function septimoPrograma() {
+  for (const color of colores) {
+    console.log(color);
+  };
+};
+//septimoPrograma();
+
+/*---------------------Ejercicio 8------------------------*/
+let numeros = [5, 7, 10, 13, 17];
+function octavoPrograma() {
+  for (const num of numeros) {
+    console.log(`El número es ${num} y su doble es ${num*2}`);
+  };
+};
+//octavoPrograma();
+
+/*---------------------Ejercicio 9------------------------*/
+let familia = [
+  {
+    apellido: "Carrion",
+    edad: 21,
+    integrante: "Hermana",
+    nombre: "Evelin"
+  },
+  {
+    apellido: "Carrion",
+    edad: 34,
+    integrante: "Hermana",
+    nombre: "carla"
+  },
+  {
+    apellido: "Garcia",
+    edad: 62,
+    integrante: "Madre",
+    nombre: "Gilda"
+  },
+  {
+    apellido: "Carrion",
+    edad: 32,
+    integrante: "Hermano",
+    nombre: "Israel"
+  }
+];
+function saludo (contenedorFamilia){
+  for (let familiar of contenedorFamilia)
+  console.log(`Hola soy ${familiar.nombre} ${familiar.apellido} (${familiar.integrante}) y tengo ${familiar.edad}`)
+}
+//saludo(familia);
+
+/*---------------------Ejercicio 10------------------------*/
+let objetoEjemplo = {
+  nombre: "Juan",
+  apellido: "Perez",
+  esColombiano: true,
+  edad: 28,
+  estudios:["primarios", "secundarios", "terciarios"]
+}
+function decimoPrograma(ejemplo) {
+  for (let key in ejemplo) {
+    console.log(key)
+  }
+}
+//decimoPrograma(objetoEjemplo);
+
+/*---------------------Ejercicio 11------------------------*/
+function programaOnce (ejemplo2){
+  for (let key in ejemplo2){
+    console.log(ejemplo2[key]);
+  };
+};
+//programaOnce (objetoEjemplo);
+
+/*---------------------Ejercicio 12------------------------*/
+function programaDoce(){
+  let num = Number(prompt("Ingrese un numero"));
+  let numPares = 0;
+  let numImpares = 0;
+  while(num > 0){
+    if(num % 2 == 0){
+      numPares += num;
+      num = Number(prompt("Ingrese un numero"));
+    }else{
+      numImpares += num;
+      num = Number(prompt("Ingrese un numero"));
+    }
+  }
+  console.log(`Pares: ${numPares}`);
+  console.log(`Impares: ${numImpares}`);
+}
+//programaDoce();
+
+/*---------------------Ejercicio 13------------------------*/
+let conjuntoNumeros = [32, 78, 91, 44, 86, 62, 27, 104, 12, 59];
+function mayor(numeros) {
+  let numMayor = 5;
+  for(let aux of numeros){
+    if(aux > numMayor){
+      numMayor = aux;
+    }
+  }
+  console.log(`El numero más grande es ${numMayor}`);
+}
+mayor(conjuntoNumeros);
